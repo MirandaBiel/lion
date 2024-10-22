99fedd027e9979109c8a6c923f45d735aa0ced93
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2024-10-21 22:05:08 -0300

    Backups Main Window

---
+++ 0-run.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from modulos.super_main_window import SuperMainWindow
+
+# Cria a aplicação
+app = QApplication(sys.argv)
+
+# Instancia a janela principal da aplicação
+window = SuperMainWindow()
+
+# Exibe a janela
+window.show()
+
+# Inicia a câmera
+window.start_camera()
+
+# Executa o loop da aplicação
+sys.exit(app.exec_())
