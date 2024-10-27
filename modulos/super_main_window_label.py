27e159baac351e1e22c63a337ff44bb30acaf57a
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2024-10-27 20:22:26 -0300

    troca da main window

---
+++ modulos/super_main_window_label.py
@@ -0,0 +1,73 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5 import QtGui, QtWidgets
+# from picamera2 import Picamera2
+# from picamera2.previews.qt import QGlPicamera2
+import sys
+from py_GUIs.main_window_label import Ui_MainWindow
+
+
+# Importa as classes correspondentes de cada janela da pasta 'modulos'
+from modulos.super_foco import SuperFoco
+from modulos.super_config import SuperConfig
+from modulos.super_advanced_settings import SuperAdvancedSettings
+from modulos.super_results import SuperResults
+
+# Classe principal que expande a janela principal da aplicação
+class SuperMainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(SuperMainWindow, self).__init__(*args, **kwargs)
+        
+        # Inicializa a interface gerada pelo Qt Designer
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Inicializando as janelas filhas e passando `self` como referência para `main_window`
+        self.advanced_settings_window = SuperAdvancedSettings(self)
+        self.config_window = SuperConfig(self)
+        self.foco_window = SuperFoco(self)
+        self.results_window = SuperResults(self)
+
+        # Conecta os itens do menubar com os métodos que abrem as janelas
+        self.ui.menuFoco.triggered.connect(self.open_foco_window)
+        self.ui.menuConfigura_es.triggered.connect(self.open_config_window)
+        self.ui.menuConfigura_es_avan_adas.triggered.connect(self.open_advanced_settings_window)
+        self.ui.menuResultados.triggered.connect(self.open_results_window)
+        
+    def start_camera(self):
+        # Método para iniciar a câmera
+        if self.ui.picam2 is not None:
+            self.ui.picam2.start()
+
+    def stop_camera(self):
+        # Método para parar a câmera
+        if self.ui.picam2 is not None:
+            self.ui.picam2.stop()
+
+    # Métodos para abrir cada janela
+    def open_foco_window(self):
+        self.hide()
+        self.foco_window.showMaximized()
+
+    def open_config_window(self):
+        self.hide()
+        self.config_window.showMaximized()
+
+    def open_advanced_settings_window(self):
+        print('ok')
+        self.hide()
+        self.advanced_settings_window.showMaximized()
+
+    def open_results_window(self):
+        self.hide()
+        self.results_window.showMaximized()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    # Cria uma instância da janela principal
+    main_window = SuperMainWindow()
+    main_window.show()
+
+    sys.exit(app.exec_())
--- run_teste.py
+++ run_teste.py
@@ -1,15 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from modulos.super_results import SuperResults
+from modulos.super_main_window_label import SuperMainWindow
 
 # Cria a aplicação
 app = QApplication(sys.argv)
 
 # Instancia a janela principal da aplicação
-window = SuperResults()
+window = SuperMainWindow()
 
 # Exibe a janela
 window.showMaximized()
+
+# Inicia a câmera
+#window.start_camera()
 
 # Executa o loop da aplicação
 sys.exit(app.exec_())
