0555487d131c4ebef5923492a32d5f9143d015d4
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2024-10-29 11:32:21 -0300

    imshow ok

--- teste.py
+++ teste.py
@@ -2,16 +2,16 @@ from picamera2 import Picamera2
 from collections import deque
 import cv2
 
-# Parâmetros da captura
+# ParÃ¢metros da captura
 fs = 30  # Taxa de quadros em Hz
-ts = 5   # Duração da captura em segundos
-n_frames = fs * ts + 10  # Número total de quadros a capturar
+ts = 5   # DuraÃ§Ã£o da captura em segundos
+n_frames = fs * ts + 10  # NÃºmero total de quadros a capturar
 
-# Cria a fila com tamanho máximo
+# Cria a fila com tamanho mÃ¡ximo
 queue = deque(maxlen=n_frames)
 tempos_de_captura = []
 
-# Configura a câmera
+# Configura a cÃ¢mera
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration(
     controls={"FrameDurationLimits": (33333, 33333)},  # Limita para 30 fps
@@ -22,9 +22,9 @@ picam2.start()
 
 # Captura e armazena os quadros na fila
 for _ in range(n_frames):
-    main, metadata = picam2.capture_arrays()
+    [main], metadata = picam2.capture_arrays()
     tempos_de_captura.append(metadata["SensorTimestamp"])
-    queue.append(main)  # Adiciona o quadro à fila
+    queue.append(main)  # Adiciona o quadro Ã  fila
 
 # Calcula os intervalos entre os tempos de captura
 intervalos = [(((tempos_de_captura[i+1] - tempos_de_captura[i]) * 0.000001) - 33.333)
