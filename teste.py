4cf400a32e9a6e3b07d2610647cf8cd0ad4d53c1
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2024-10-29 10:51:09 -0300

    teste capture

--- teste.py
+++ teste.py
@@ -1,20 +1,19 @@
-from picamera2.outputs import FileOutput
-from picamera2.encoders import Encoder
 from picamera2 import Picamera2
+from collections import deque
 import io
-import time
 
+fs = 30 # Hz
+ts = 5 # s
+n_frames = fs * ts + 10
+
+queue = deque(maxlen=n_frames)
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"size": (800, 600)})
+video_config = picam2.create_video_configuration(controls={"FrameDurationLimits": (33333, 33333)}, main={"size": (800, 600)})
 picam2.configure(video_config)
+picam2.start()
 
-encoder = Encoder()
-buffer = io.BytesIO()
-output = FileOutput(buffer)
+for _ in range(2):
+    main, metadata = picam2.capture_arrays()
 
-picam2.start_recording(encoder, output)
-time.sleep(5)
-picam2.stop_recording()
-
-print("PROCESSO FINALIZADO")
+picam2.stop()
