593aa2858e18c305f6d069fa69308886137e7ff7
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2024-11-02 10:33:52 -0300

    armazenando no buffer

--- teste.py
+++ teste.py
@@ -1,18 +1,20 @@
 from picamera2.outputs import FileOutput
 from picamera2.encoders import Encoder
+from picamera2 import Picamera2
+from PyQt5 import QtCore, QtGui, QtWidgets
 import io
 import time
 
 
 picam2 = Picamera2()
-video_config = self.ui.picam2.create_video_configuration(
+video_config = picam2.create_video_configuration(
             controls={"FrameDurationLimits": (33333, 33333)},  # Limita para 30 fps
-            main={"size": (800, 600)}
+            main={'format': 'XBGR8888', "size": (800, 600)}
         )
 picam2.configure(video_config)
 
 encoder = Encoder()
-buffer = io.Bytes()
+buffer = io.BytesIO()
 output = FileOutput(buffer)
 
 picam2.start_recording(encoder, output)
